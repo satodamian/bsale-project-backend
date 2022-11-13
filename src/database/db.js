15057b7aecfbabf6328c387c73411e6617e192fe
SatoDQ <satowird93@gmail.com>
2022-11-13 18:39:47 -0500

    Correccion PORT

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,9 @@
 //Definimos la conexion
 import Sequelize from "sequelize";
-import {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER} from '../config.js'
+import {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT} from '../config.js'
 
 export const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
+  port: Number(DB_PORT),
   host: DB_HOST,
   dialect: "mysql",
   logging: false,
