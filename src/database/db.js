873e9a3f02150cf4d99fd4c0d8c496b9231576e7
SatoDQ <satowird93@gmail.com>
2022-11-13 18:46:49 -0500

    Pool Add Configuration

--- src/database/db.js
+++ src/database/db.js
@@ -7,5 +7,11 @@ export const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
   logging: false,
+  pool:{
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
