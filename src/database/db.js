cfea1ada2caa0a7474cc40ad035223a96a32479d
SatoDQ <satowird93@gmail.com>
2022-11-13 17:24:35 -0500

    PORT

--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,7 @@ import {DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER} from '../config.js'
 export const db = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: "mysql",
-  port: DB_PORT,
+  // port: DB_PORT,
   logging: false,
 });
 
