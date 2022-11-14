06ca059c75123f30df746086588f3c7e08dd6e93
SatoDQ <satowird93@gmail.com>
2022-11-13 19:37:07 -0500

    Cors agregado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import BSaleRoutes from '../routes/bsale.routes.js'
-
+import cors from 'cors'
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(BSaleRoutes);
 
